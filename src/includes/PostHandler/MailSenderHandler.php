90d3ada76338e9e4ff7b4814310b8fd560a9bad0
green spot <in.green.spot@gmail.com>
2025-12-06 00:49:16 +0900

    update

--- src/includes/PostHandler/MailSenderHandler.php
+++ src/includes/PostHandler/MailSenderHandler.php
@@ -14,87 +14,87 @@ class MailSenderHandler extends AbstractHandler {
 
   public function __construct(array $config = []) {
     $this->config = $config;
-    $this->mailer = new PHPMailer(true);
-    $this->mailer->Timeout = 5;
-
-    // Charset
-    $this->mailer->CharSet = $config['charset'] ?? 'UTF-8';
-
-    // SMTP
-    if(isset($config['smtp'])) {
-      $smtp = $config['smtp'];
-      $this->mailer->isSMTP();
-      $this->mailer->Host = $smtp['host'] ?? '';
-      $this->mailer->SMTPAuth = $smtp['auth'] ?? true;
-      $this->mailer->Username = $smtp['username'] ?? '';
-      $this->mailer->Password = $smtp['password'] ?? '';
-      $this->mailer->SMTPSecure = $smtp['secure'] ?? PHPMailer::ENCRYPTION_STARTTLS;
-      $this->mailer->Port = $smtp['port'] ?? 587;
-    } else {
-      $this->mailer->isMail();
-    }
-
-    // From
-    if(!empty($config['from'])) {
-      $this->mailer->setFrom($config['from'], $config['fromName'] ?? '');
-    }
-
-    // Reply-To
-    if(!empty($config['replyTo'])) {
-      $this->mailer->addReplyTo(
-        $config['replyTo'] ?? '',
-        $config['replyToName'] ?? ''
-      );
-    }
-
-    // CC
-    if(!empty($config['cc'])) {
-      foreach($config['cc'] as $cc) {
-        $this->mailer->addCC($cc);
-      }
-    }
-
-    // BCC
-    if(!empty($config['bcc'])) {
-      foreach($config['bcc'] as $bcc) {
-        $this->mailer->addBCC($bcc);
-      }
-    }
-
-    // Subject
-    if(!empty($config['subject'])) {
-      $this->mailer->Subject = $config['subject'];
-    }
-
-    // To
-    if(!empty($config['to'])) {
-      $this->mailer->addAddress($config['to'], $config['toName'] ?? '');
-    }
   }
+
   public function handle(Form $form, HandlerPipelineContext $context): HandlerResult {
     $data = $form->getNormalizedData();
 
-    // Body
-    if(!empty($this->config['bodyTemplate'])) {
-      $twig = $this->getTwig();
-      $template = $twig->createTemplate($this->config['bodyTemplate']);
-      $this->mailer->Body = $template->render(['data' => $data]);
-    }
-
-    // To field
-    if(!empty($this->config['toField']) && isset($data[$this->config['toField']])) {
-      $this->mailer->addAddress($data[$this->config['toField']]);
-    }
-
-    // Send email
-    $this->mailer->send();
-    /*
     try {
+      $this->mailer = new PHPMailer(true);
+      $this->mailer->Timeout = $this->config["timeout"] ?? 60;
+
+      // Charset
+      $this->mailer->CharSet = $this->config["charset"] ?? "UTF-8";
+
+      // SMTP
+      if(isset($this->config["smtp"])) {
+        $smtp = $this->config["smtp"];
+        $this->mailer->isSMTP();
+        $this->mailer->Host = $smtp["host"] ?? "";
+        $this->mailer->SMTPAuth = $smtp["auth"] ?? true;
+        $this->mailer->Username = $smtp["username"] ?? "";
+        $this->mailer->Password = $smtp["password"] ?? "";
+        $this->mailer->SMTPSecure = $smtp["secure"] ?? PHPMailer::ENCRYPTION_STARTTLS;
+        $this->mailer->Port = $smtp["port"] ?? 587;
+      } else {
+        $this->mailer->isMail();
+      }
+
+      // From
+      if(!empty($this->config["from"])) {
+        $this->mailer->setFrom($this->config["from"], $this->config["fromName"] ?? "");
+      }
+
+      // Reply-To
+      if(!empty($this->config["replyTo"])) {
+        $this->mailer->addReplyTo(
+          $this->config["replyTo"] ?? "",
+          $this->config["replyToName"] ?? ""
+        );
+      }
+
+      // CC
+      if(!empty($this->config["cc"])) {
+        foreach($this->config["cc"] as $cc) {
+          $this->mailer->addCC($cc);
+        }
+      }
+
+      // BCC
+      if(!empty($this->config["bcc"])) {
+        foreach($this->config["bcc"] as $bcc) {
+          $this->mailer->addBCC($bcc);
+        }
+      }
+
+      // Subject
+      if(!empty($this->config["subject"])) {
+        $this->mailer->Subject = $this->config["subject"];
+      }
+
+      // To
+      if(!empty($this->config["to"])) {
+        $this->mailer->addAddress($this->config["to"], $this->config["toName"] ?? "");
+      }
+
+      // Body
+      if(!empty($this->config["bodyTemplate"])) {
+        $twig = $this->getTwig();
+        $template = $twig->createTemplate($this->config["bodyTemplate"]);
+        $this->mailer->Body = $template->render(["data" => $data]);
+      }
+
+      // To field
+      if(!empty($this->config["toField"]) && isset($data[$this->config["toField"]])) {
+        $this->mailer->addAddress($data[$this->config["toField"]]);
+      }
+
+      // Send email
       $this->mailer->send();
-    } catch (\Exception $e) {
-      // Handle error (log it, rethrow it, etc.)
+
+    }catch(\PHPMailer\PHPMailer\Exception $e) {
+      return $this->failure($this->mailer->ErrorInfo, ["errorObject" => $e]);
     }
-      */
 
     return $this->success();
   }
